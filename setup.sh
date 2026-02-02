2c26e92ca5a7a27cb7993e93e6882b6ee3c92895
xmuhuanglab <95668602+xmuhuanglab@users.noreply.github.com>
2026-02-02 16:57:36 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# 创建环境
-micromamba env create -n eScreen -f escreen.yaml -y
+# create environment
+conda env create -n eScreen -f escreen.yaml -y
+conda run -n eScreen pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu118
+conda install -n eScreen -c nvidia cuda-nvcc cuda-cudart-dev -y
 
-# 所有命令都在指定环境中执行
-micromamba run -n eScreen pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu118
-micromamba install -n eScreen -c nvidia cuda-nvcc cuda-cudart-dev -y
-
-# 使用micromamba run执行所有pip安装
-micromamba run -n eScreen bash -c '
+# Using conda run to conduct all pip install pipeline
+conda run -n eScreen bash -c '
     mkdir flash-attn
     cd flash-attn
     wget https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.3/flash_attn-2.7.3+cu11torch2.6cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
