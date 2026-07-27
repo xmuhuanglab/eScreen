0ed7aff367edc434e1981bd38b632daa6fe0fdc5
Liquan Lin <1450633396@qq.com>
2026-07-27 18:44:09 +0800

    删去不需要的文件

--- escreen/SH2core/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from .AttentionBlock import AttentionBlock
-from .HyenaCascade import HyenaCascade
-from .ParallelGatedConvBlock import ParallelGatedConvBlock
-from .ParallelGatedMLP import ParallelGatedMLP
-from .RMSNorm import RMSNorm
-from .HyenaInferenceEngine import HyenaInferenceEngine
-
-__all__ = ["AttentionBlock", "HyenaCascade", "ParallelGatedConvBlock", "ParallelGatedMLP", "RMSNorm"]
